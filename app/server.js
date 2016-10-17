51a29bae3e70f042a7b29d9d26e3a3a0029cd079
Brad Schlintz <bschlintz@live.com>
2016-10-16 19:06:55 -0500

    added server.js back into the mix

--- app/server.js
+++ app/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var watcher = require('./app/watcher');
 
 app.post('/', function (req, res) {
     res.send('POST received!');
@@ -8,6 +9,7 @@ app.post('/', function (req, res) {
 var Start = function(){
     app.listen(3000, function () {
         console.log('Running on :3000');
+        watcher.checkForRequests(false, 5000);
     });
 } 
 
