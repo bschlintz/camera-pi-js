f23afaa36d934cda1c68059d498978e32ec60930
Brad Schlintz <bschlintz@live.com>
2016-11-01 23:31:12 -0500

    Added led panel logic using onoff module

---
+++ app/led-panel/index.js
@@ -0,0 +1,36 @@
+// https://github.com/fivdi/onoff
+
+var Gpio = require('onoff').Gpio;
+
+var ledPanel = {};
+ledPanel.leds = {
+    GREEN: 5,
+    YELLOW: 6,
+    RED: 13
+}
+
+ledPanel.toggle = function(pin, state) {
+    console.log(`LED: ${pin}  STATE: ${state === undefined ? "toggle" : state}`);
+    var led = _leds[pin];
+    if(state === undefined){
+        led.writeSync(led.readSync() ^ 1);
+    } else {
+        led.writeSync(state ? 0 : 1);
+    }
+}
+
+ledPanel.dispose = function() {
+    Object.keys(_leds).forEach(k => _leds[k].unexport());
+}
+
+var _leds = {};
+var _isInitialized = false;
+var _init = function(){
+    if (!_isInitialized){
+        Object.keys(ledPanel.leds).forEach(k => _leds[ledPanel.leds[k]] = new Gpio(ledPanel.leds[k], 'out'));
+        _isInitialized = true;
+    }
+};
+
+_init();
+module.exports = ledPanel;
--- debug.js
+++ debug.js
@@ -1,4 +1,48 @@
-var createGuid = require('node-uuid').v4;
-var droopyIot = require('droopy-iot');
-var iot = droopyIot.register("bs-debugger");
-iot.trigger("take-pic", { id: createGuid() }, "mke-room1")
+// var createGuid = require('node-uuid').v4;
+// var droopyIot = require('droopy-iot');
+// var iot = droopyIot.register("bs-debugger");
+// iot.trigger("take-pic", { id: createGuid() }, "mke-room1")
+
+var ledPanel = require('./app/led-panel');
+
+ledPanel.toggle(ledPanel.leds.GREEN, false);
+ledPanel.toggle(ledPanel.leds.RED, false);
+ledPanel.toggle(ledPanel.leds.YELLOW, false);
+
+// ledPanel.toggle(ledPanel.leds.GREEN, true);
+// ledPanel.toggle(ledPanel.leds.RED, true);
+// ledPanel.toggle(ledPanel.leds.YELLOW, true);
+
+var gIv, yIv, rIv;
+
+gIv = setInterval(function () {
+  ledPanel.toggle(ledPanel.leds.GREEN);
+}, 200);
+
+setTimeout(() => {
+    yIv = setInterval(function () {
+        ledPanel.toggle(ledPanel.leds.YELLOW);
+    }, 200);
+}, 50)
+
+setTimeout(()=>{
+    rIv = setInterval(function () {
+        ledPanel.toggle(ledPanel.leds.RED);
+    }, 200);
+}, 100)
+
+// Stop blinking the LED and turn it off after 5 seconds.
+setTimeout(function () {
+    // Stop blinking
+    clearInterval(gIv); 
+    clearInterval(yIv);
+    clearInterval(rIv);
+    
+    // Turn LEDs off
+    ledPanel.toggle(ledPanel.leds.GREEN, false);
+    ledPanel.toggle(ledPanel.leds.YELLOW, false);
+    ledPanel.toggle(ledPanel.leds.RED, false);
+
+    // Unexport GPIO and free resources
+    ledPanel.dispose();
+}, 30000);
